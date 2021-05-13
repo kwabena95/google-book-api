e391876790a29c5fd29be4751493ac5e6dec51dc
ebenezer <e.kankam95@hotmail.com>
2021-05-13 10:03:32 -0500

    implemented apollo server and applied it to express server as middleware

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const path = require('path');
-const db = require('./config/connection');
+const connectDB = require('./config/connection');
 const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-express');
+
+require('dotenv').config();
+
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
@@ -16,6 +30,11 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+connectDB.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    // log where we can go to test our GQL API
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    console.log(`MongoDB Connected`)
+  });
 });
